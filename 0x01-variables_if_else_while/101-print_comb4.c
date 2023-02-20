871e55d7f576342c5a074ffd7195b1ecc9171009
Dani <chukwujidanielc@gmail.com>
2023-02-19 16:37:31 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 /**
- * main = Entry point
+ * main - Entry point
  * Return: Always 0 (Success)
  */
 
@@ -14,10 +14,10 @@ int main(void)
 
 	for (d = '0'; d < '9'; d++)
 	{
-	
+
 	for (p = d + 1; p <= '9'; p++)
 	{
-	
+
 	for (q = p + 1; q <= '9'; q++)
 	{
 	if ((p != d) != q)
